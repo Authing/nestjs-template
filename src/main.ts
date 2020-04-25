13e6fefde896d7962e826236677c94da5e28e02d
cj <1066983132@qq.com>
2020-04-25 10:22:44 +0800

    feat: Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  // The available properties of configuration object are described here: 
+  // https://github.com/expressjs/cors#configuration-options
+  app.enableCors()
+
   await app.listen(3000);
 }
 bootstrap();
