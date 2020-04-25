e51e2ad165baf6b4322757d2bc67279628960e18
cj <1066983132@qq.com>
2020-04-25 11:46:03 +0800

    feat: Steup TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import * as path from 'path'
 import { DateFormatDirective } from './graphql/directives/formatDate';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 function getEnvFile() {
   switch (process.env.NODE_ENV) {
@@ -19,6 +20,10 @@ function getEnvFile() {
   }
 }
 
+require('dotenv').config({
+  path: getEnvFile()
+})
+
 @Module({
   imports: [
     // NestJS Config Module: https://docs.nestjs.com/techniques/configuration
@@ -31,6 +36,7 @@ function getEnvFile() {
       isGlobal: true,
     }),
 
+    // Doc: https://docs.nestjs.com/graphql/quick-start
     GraphQLModule.forRoot({
       cors: false,
 
@@ -44,6 +50,20 @@ function getEnvFile() {
 
       // 生产环境关闭 debug 选项
       debug: process.env.NODE_ENV !== "prod"
+    }),
+
+    // TypeROM: https://docs.nestjs.com/techniques/database
+    TypeOrmModule.forRoot({
+      // @ts-ignore
+      type: process.env.DATABASE_TYPE,
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DBNAME,
+      // 加载 src 目录下所有 .entity.ts/.entity.js 后缀的文件
+      entities: [path.resolve(__dirname, '**', '*.entity{.ts,.js}')],
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
